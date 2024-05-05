73773317d6cf74bc5584135973f1d8be351938fa
rcroughs <romain.croughs@gmail.com>
2024-05-05 17:59:53 +0200

    catting multiple files

--- src/cat.rs
+++ src/cat.rs
@@ -3,15 +3,10 @@ use std::fs;
 use std::process::exit;
 
 fn main() {
-    let file_path : String = env::args().nth(1).unwrap_or_else(|| {
-        eprintln!("No path found");
-        exit(1);
-    });
-
-    let buffer : String =  fs::read_to_string(file_path).unwrap_or_else(|e| {
-        eprintln!("{e}");
-        exit(1);
-    });
-
-    print!("{buffer}\n");
+    for path in env::args().skip(1) {
+        println!("{}",fs::read_to_string(path).unwrap_or_else(|e| {
+            eprintln!("{e}");
+            exit(1);
+        }));
+    }
 }
