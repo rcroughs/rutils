6511e0d20c4d289f713e8a5777859b9fa9cf428a
rcroughs <romain.croughs@gmail.com>
2024-05-05 18:00:15 +0200

    ls -la

--- src/ls.rs
+++ src/ls.rs
@@ -1,20 +1,56 @@
-use std::{env, fs, io, path::PathBuf, str::FromStr};
+use std::{env, ffi::OsStr, fs, io, path::PathBuf, process::exit, str::FromStr};
 
-fn directory_elements(path: String) -> io::Result<Vec<PathBuf>> {
+struct Params {
+    list: bool,
+    all: bool
+}
+
+fn directory_elements(path: PathBuf) -> io::Result<Vec<PathBuf>> {
     let dir = fs::read_dir(path)?;
     let res : Vec<PathBuf> = dir.filter_map(|elem| Some(elem.ok()?.path())).collect();
     Ok(res)
 }
 
-fn main() {
-    let path : String = match env::args().nth(1) {
-        Some(s) => s,
-        None => String::from_str(".").unwrap()
+fn parse_args() -> Params {
+    let mut params : Params = Params {
+        list: false,
+        all: false,
     };
-    let elements : Vec<PathBuf> = directory_elements(path).unwrap();
-
-    for element in elements {
-        let s : &str = element.to_str().unwrap();
-        println!("{s}");
+    for arg in env::args().skip(1) {
+        if arg.starts_with("-") {
+            if arg.contains("l") {
+                params.list = true;
+            }
+            if arg.contains("a") {
+                params.all = true;
+            }
+        }
     }
+    return params;
+}
+
+fn show_elements(elements: Vec<PathBuf>, all: bool, list: bool) {
+    for element in elements {
+        if !element.file_name().unwrap().to_str().unwrap().starts_with(".") || all {
+            if element.metadata().unwrap().is_dir() {
+                print!("\x1b[94m");
+            }
+            let s : &OsStr = element.file_name().unwrap();
+            print!("{}", s.to_str().unwrap());
+            print!("\x1b[0m  ");
+        }
+        if list {
+            println!();
+        }
+    }
+    print!("\n")
+}
+
+fn main() {
+    let params : Params = parse_args();
+    let path : PathBuf = env::current_dir().unwrap_or_else(|e| {
+        eprintln!("{e}");
+        exit(1);
+    });
+    show_elements(directory_elements(path).unwrap(), params.all, params.list);
 }
