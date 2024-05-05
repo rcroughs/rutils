c67de00c75e113644ebb3fa9037b718abaec4300
rcroughs <romain.croughs@gmail.com>
2024-05-05 18:24:30 +0200

    adding unix perms

--- src/ls.rs
+++ src/ls.rs
@@ -1,4 +1,4 @@
-use std::{env, ffi::OsStr, fs, io, path::PathBuf, process::exit, str::FromStr};
+use std::{env, ffi::OsStr, fs::{self, Metadata, Permissions}, io, os::unix::fs::PermissionsExt, path::PathBuf, process::exit, str::FromStr};
 
 struct Params {
     list: bool,
@@ -29,9 +29,35 @@ fn parse_args() -> Params {
     return params;
 }
 
+fn show_metadata(metadata: Metadata) {
+    if metadata.is_dir() {
+        print!("d")
+    } else {
+        print!("-")
+    }
+    let mode : u32 = metadata.permissions().mode();
+    for i in (0..9).rev() {
+        if mode & (1 << i) != 0 {
+            if i % 3 == 0 {
+                print!("x")
+            } else if i % 3 == 1 {
+                print!("w")
+            } else {
+                print!("r")
+            }
+        } else {
+            print!("-")
+        }
+    }
+    print!(" ");
+}
+
 fn show_elements(elements: Vec<PathBuf>, all: bool, list: bool) {
     for element in elements {
         if !element.file_name().unwrap().to_str().unwrap().starts_with(".") || all {
+            if list {
+                show_metadata(element.metadata().unwrap());
+            }
             if element.metadata().unwrap().is_dir() {
                 print!("\x1b[94m");
             }
